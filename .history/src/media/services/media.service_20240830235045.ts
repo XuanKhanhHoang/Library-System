aec54e4335293b6b636df6faec0948edc3cd2676
hoangxuankhanh <hoangxuankhanh.work@gmail.com>
2024-09-01 15:12:03 +0700

    delete .history

--- .history/src/media/services/media.service_20240830235045.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'nestjs-prisma';
-
-@Injectable()
-export class MediaService {
-  constructor(private prismaService: PrismaService) {}
-}
